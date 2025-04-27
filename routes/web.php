781fd50476a676047c73f3f5acaf2765e1d9375d
Putri Aulia <putriauliamufidaa@gmail.com>
2025-04-27 13:43:23 +0700

    Percobaan 2

--- routes/web.php
+++ routes/web.php
@@ -23,16 +23,4 @@ Route::post('/user/store', [UserController::class, 'store']) ->name('user.store'
 Route::get('/contact', [ContactController::class, 'showForm'])->name('contact.form');
 Route::post('/contact', [ContactController::class, 'submitForm'])->name('contact.submit');
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
-
-Route::middleware(['auth'])->group(function () {
-    Route::redirect('settings', 'settings/profile');
-
-    Volt::route('settings/profile', 'settings.profile')->name('settings.profile');
-    Volt::route('settings/password', 'settings.password')->name('settings.password');
-    Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
-});
-
 require __DIR__.'/auth.php';
